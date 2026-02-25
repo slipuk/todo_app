b212941f365a16855de61fe6a07bc27d96418d2f
slipuk <205234930+slipuk@users.noreply.github.com>
2026-02-25 14:51:28 +0000

    fix "no file or directory" error
    app will create directory "data" and file "todo.db" all by itself

--- main.py
+++ main.py
@@ -6,23 +6,23 @@ def main():
 	connection = get_connection()
 	repository = TaskRepository(connection)
 	repository.create_table()
-
 	service = TaskService(repository)
 
-	# service.create_task("Hooking", "for bed")
-	# service.delete_task(1)
-	# service.edit_task(1, "pasta")
-	
-	# print(service.find_task(1))
+	while True:
+		print("todo app")
 
-	# task = service.get_task(1)
+		print("create task press 1")
 
-	# print(f"{task.id} | {task.name} | {task.description}")
+		user_input = input(">")
+		if user_input.isnumeric() and int(user_input) == 1:
+			user_input_name = input("Enter a name\n>")
+			user_input_description = input("enter a description (blank for none)\n>")
+			service.create_task(user_input_name, user_input_description)
 	
-	tasks = service.get_all_tasks()
+		tasks = service.get_all_tasks()
 	
-	for task in tasks:
-		print(f"{task.id} | {task.name} | {task.description}")
+		for task in tasks:
+			print(f"{task.id} | {task.name} | {task.description}")
 
 if __name__ == "__main__":
 	main()
--- source/database.py
+++ source/database.py
@@ -2,11 +2,25 @@
 
 import sqlite3
 from pathlib import Path
+import os
 
 #allows to get base dir
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 #gets database path
+if not os.path.exists(f"{BASE_DIR}/data"):
+	os.makedirs(f"{BASE_DIR}/data")
+	print("Folder created successfully.")
+else:
+	print("Folder already exists.")
+
+if not os.path.exists(f"{BASE_DIR}/data/todo.db"):
+	with open(f"{BASE_DIR}/data/todo.db", "w") as file:
+		file.write("")
+	print("File created successfully.")
+else:
+	print("File already exists")
+
 DATABASE_PATH = BASE_DIR / "data" / "todo.db"
 
 #connects with the database by path 
